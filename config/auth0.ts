be838ac5e2412f3a8299b62c58a2e903be988c2a
Matthias Hiltpold <pygmalion@daedalus.local>
2021-03-16 23:10:11 +0100

    added config

---
+++ config/auth0.ts
@@ -0,0 +1,6 @@
+// TODO: cannot cmmit this file, use something like below
+export default {
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN ?? "",
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID ?? "",
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET ?? ""
+}
