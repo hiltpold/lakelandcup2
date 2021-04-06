68526ef838a3084c16d4b0ca3f24cdd4599bbbfd
hiltpold <m.hiltpold@protonmail.ch>
2021-04-06 22:42:44 +0200

    started rebuilding the repo structure

---
+++ frontend/get-token.sh
@@ -0,0 +1,4 @@
+curl --request POST \
+  --url https://dev-7hxvk2ig.us.auth0.com/oauth/token \
+  --header 'content-type: application/json' \
+  --data '{"client_id":"uiuUUo60eT0o6pB2LT4PnhwlPF3SzATp","client_secret":"a98YYlU4OBhlkeLIKhxEk8ILPIL_7ZUsNjL7MNBAArVZaU7g4VJh0EjDeiTwnR2R","audience":"https://dev-7hxvk2ig.us.auth0.com/api/v2/","grant_type":"client_credentials"}'
